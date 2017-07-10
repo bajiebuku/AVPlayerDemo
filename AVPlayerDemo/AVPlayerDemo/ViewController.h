803e059ce0f2d0a89bfb7befcfef878c5a342e41
千叶 <dongdong12590@163.com>
2017-07-10 14:14:40 +0800

    项目准备

---
+++ AVPlayerDemo/AVPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVPlayerDemo
+//
+//  Created by 韩东 on 17/7/10.
+//  Copyright © 2017年 HD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AVPlayerDemo/AVPlayerDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AVPlayerDemo
+//
+//  Created by 韩东 on 17/7/10.
+//  Copyright © 2017年 HD. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
