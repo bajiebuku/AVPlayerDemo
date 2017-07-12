bf38f8acdd6570e1bb864e22e7c168b6bd551a38
千叶 <dongdong12590@163.com>
2017-07-12 10:09:16 +0800

    项目准备+视频源准备+页面布局

--- AVPlayerDemo/AVPlayerDemo/ViewController.m
+++ AVPlayerDemo/AVPlayerDemo/ViewController.m
@@ -7,8 +7,26 @@
 //
 
 #import "ViewController.h"
+#import "Masonry.h"
+#import <AVFoundation/AVFoundation.h>
+
+#define kScreenWidth [UIScreen mainScreen].bounds.size.width
+#define kScreenHeight [UIScreen mainScreen].bounds.size.height
 
 @interface ViewController ()
+
+@property (strong, nonatomic) UIView *backView;
+@property (nonatomic,strong) AVPlayer *player;
+@property (nonatomic,strong) AVPlayerItem *playerItem;
+@property (nonatomic,strong) AVPlayerLayer *playerLayer;
+// 底部BottmView
+@property (nonatomic,strong) UIView *bottomView;
+@property (nonatomic,strong) UIButton *playButton;
+@property (nonatomic,strong) UIButton *fullScreenButton;
+@property (nonatomic,strong) UIProgressView *progressView;
+@property (nonatomic,strong) UISlider *slider;
+@property (nonatomic,strong) UILabel *nowLabel;
+@property (nonatomic,strong) UILabel *remainLabel;
 
 @end
 
@@ -16,14 +34,114 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self setupUI];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setupUI{
+    
+    self.backView = [[UIView alloc] initWithFrame:CGRectMake(0, 80, kScreenWidth, kScreenHeight / 2.5)];
+    self.backView.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:self.backView];
+    
+    // 初始化播放器item
+    self.playerItem = [[AVPlayerItem alloc] initWithURL:[NSURL URLWithString:@"http://flv2.bn.netease.com/videolib3/1608/30/zPuaL7429/SD/zPuaL7429-mobile.mp4"]];
+    self.player = [[AVPlayer alloc] initWithPlayerItem:self.playerItem];
+    // 初始化播放器的Layer
+    _playerLayer = [AVPlayerLayer playerLayerWithPlayer:self.player];
+    _playerLayer.frame = self.backView.bounds;
+    [self.backView.layer insertSublayer:self.playerLayer atIndex:0];
+    
+    // 布局底部功能栏
+    self.bottomView = [[UIView alloc] init];
+    self.bottomView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.6];
+    [self.backView addSubview:self.bottomView];
+    [self.bottomView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.backView).with.offset(0);
+        make.right.equalTo(self.backView).with.offset(0);
+        make.bottom.equalTo(self.backView).with.offset(0);
+        make.height.mas_equalTo(30);
+    }];
+    
+    // 播放或暂停
+    self.playButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.playButton setImage:[UIImage imageNamed:@"play"] forState:UIControlStateNormal];
+    self.playButton.showsTouchWhenHighlighted = YES;
+    [self.bottomView addSubview:self.playButton];
+    [self.playButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.bottomView).with.offset(5);
+        make.centerY.equalTo(self.bottomView);
+        make.size.mas_equalTo(CGSizeMake(30, 30));
+    }];
+    
+    // 底部全屏按钮
+    self.fullScreenButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.fullScreenButton setImage:[UIImage imageNamed:@"fullscreen"] forState:UIControlStateNormal];
+    self.fullScreenButton.showsTouchWhenHighlighted = YES;
+    [self.bottomView addSubview:self.fullScreenButton];
+    [self.fullScreenButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.equalTo(self.bottomView).with.offset(-5);
+        make.centerY.equalTo(self.bottomView);
+        make.size.mas_equalTo(CGSizeMake(30, 30));
+    }];
+    
+    // 底部进度条
+    self.slider = [[UISlider alloc] init];
+    self.slider.minimumValue = 0.0;
+    self.slider.minimumTrackTintColor = [UIColor greenColor];
+    self.slider.maximumTrackTintColor = [UIColor clearColor];
+    self.slider.value = 0.0;
+    [self.slider setThumbImage:[UIImage imageNamed:@"dot"] forState:UIControlStateNormal];
+    [self.bottomView addSubview:self.slider];
+    [self.slider mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.left.equalTo(self.bottomView).with.offset(45);
+        make.right.equalTo(self.bottomView).with.offset(-45);
+        make.centerY.equalTo(self.bottomView);
+        
+    }];
+    
+    // 底部缓存进度条
+    self.progressView = [[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleDefault];
+    self.progressView.progressTintColor = [UIColor blueColor];
+    self.progressView.trackTintColor = [UIColor lightGrayColor];
+    [self.bottomView addSubview:self.progressView];
+    [self.progressView setProgress:0.0 animated:NO];
+    
+    [self.progressView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.slider).with.offset(0);
+        make.right.equalTo(self.slider);
+        make.height.mas_equalTo(2);
+        make.centerY.equalTo(self.slider).with.offset(1);
+    }];
+    [self.bottomView sendSubviewToBack:self.progressView];
+    
+    // 底部左侧时间轴
+    self.nowLabel = [[UILabel alloc] init];
+    self.nowLabel.textColor = [UIColor whiteColor];
+    self.nowLabel.font = [UIFont systemFontOfSize:13];
+    self.nowLabel.textAlignment = NSTextAlignmentLeft;
+    [self.bottomView addSubview:self.nowLabel];
+    [self.nowLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.slider.mas_left).with.offset(0);
+        make.top.equalTo(self.slider.mas_bottom).with.offset(0);
+        make.size.mas_equalTo(CGSizeMake(100, 20));
+    }];
+    
+    // 底部右侧时间轴
+    self.remainLabel = [[UILabel alloc] init];
+    self.remainLabel.textColor = [UIColor whiteColor];
+    self.remainLabel.font = [UIFont systemFontOfSize:13];
+    self.remainLabel.textAlignment = NSTextAlignmentRight;
+    [self.bottomView addSubview:self.remainLabel];
+    [self.remainLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.equalTo(self.slider.mas_right).with.offset(0);
+        make.top.equalTo(self.slider.mas_bottom).with.offset(0);
+        make.size.mas_equalTo(CGSizeMake(100, 20));
+    }];
+    
+    
 }
+
 
 
 @end
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AVPlayerDemo
 AVPlayerDemo
+
+demo 写的有点慢，但是功能实现的比较全些，希望多多鼓励
