2eef6fec12e5e949471771e176c011df6e3dc1d5
千叶 <dongdong12590@163.com>
2017-07-14 11:13:06 +0800

    实现拖动进度条

--- AVPlayerDemo/AVPlayerDemo/ViewController.m
+++ AVPlayerDemo/AVPlayerDemo/ViewController.m
@@ -19,7 +19,7 @@
 @property (nonatomic,strong) AVPlayer *player;
 @property (nonatomic,strong) AVPlayerItem *playerItem;
 @property (nonatomic,strong) AVPlayerLayer *playerLayer;
-// 底部BottmView
+//底部BottmView
 @property (nonatomic,strong) UIView *bottomView;
 @property (nonatomic,strong) UIButton *playButton;
 @property (nonatomic,strong) UIButton *fullScreenButton;
@@ -27,8 +27,11 @@
 @property (nonatomic,strong) UISlider *slider;
 @property (nonatomic,strong) UILabel *nowLabel;
 @property (nonatomic,strong) UILabel *remainLabel;
-// 是否全屏
+//是否全屏
 @property (nonatomic,assign) BOOL isFullScreen;
+//自动消失定时器
+@property (nonatomic,strong) NSTimer *autoDismissTimer;
+@property (nonatomic,assign) BOOL isDragSlider;
 @end
 
 @implementation ViewController
@@ -44,13 +47,17 @@
     self.backView.backgroundColor = [UIColor clearColor];
     [self.view addSubview:self.backView];
     
-    // 初始化播放器item
+    // 初始化播放器
     self.playerItem = [[AVPlayerItem alloc] initWithURL:[NSURL URLWithString:@"http://flv2.bn.netease.com/videolib3/1608/30/zPuaL7429/SD/zPuaL7429-mobile.mp4"]];
     self.player = [[AVPlayer alloc] initWithPlayerItem:self.playerItem];
-    // 初始化播放器的Layer
     _playerLayer = [AVPlayerLayer playerLayerWithPlayer:self.player];
     _playerLayer.frame = self.backView.bounds;
     [self.backView.layer insertSublayer:self.playerLayer atIndex:0];
+    
+    // 监听播放器状态变化
+    [self.playerItem addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
+    // 监听缓存大小
+    [self.playerItem addObserver:self forKeyPath:@"loadedTimeRanges" options:NSKeyValueObservingOptionNew context:nil];
     
     //添加手势动作,隐藏下面的进度条
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(singleTap:)];
@@ -90,7 +97,7 @@
         make.centerY.equalTo(self.bottomView);
         make.size.mas_equalTo(CGSizeMake(30, 30));
     }];
-    //点击全屏1
+    //点击全屏
     [self.fullScreenButton addTarget:self action:@selector(clickFullScreen:) forControlEvents:UIControlEventTouchUpInside];
     
     // 底部进度条
@@ -108,7 +115,12 @@
         make.centerY.equalTo(self.bottomView);
         
     }];
+    [self.slider addTarget:self action:@selector(sliderDragValueChange:) forControlEvents:UIControlEventValueChanged];
+    [self.slider addTarget:self action:@selector(sliderTapValueChange:) forControlEvents:UIControlEventTouchUpInside];
     
+    UITapGestureRecognizer *tapSlider = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(touchSlider:)];
+    [self.slider addGestureRecognizer:tapSlider];
+    [self.bottomView addSubview:self.slider];
     // 底部缓存进度条
     self.progressView = [[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleDefault];
     self.progressView.progressTintColor = [UIColor blueColor];
@@ -154,7 +166,7 @@
 #pragma mark - 暂停或者播放
 - (void)pauseOrPlay:(UIButton *)sender{
     
-    if (self.player.rate != 1.0f)
+    if (self.player.rate != 1)
     {
         [sender setImage:[UIImage imageNamed:@"pause"] forState:UIControlStateNormal];
         
@@ -193,19 +205,17 @@
     }else if(interfaceOrientation==UIInterfaceOrientationLandscapeRight){
         self.backView.transform = CGAffineTransformMakeRotation(M_PI_2);
     }
-    // BackView的frame能全屏
+    // BackView 全屏
     self.backView.frame = CGRectMake(0, 0, kScreenWidth, kScreenHeight);
     // layer的方向宽和高对调
     self.playerLayer.frame = CGRectMake(0, 0, kScreenHeight, kScreenWidth);
     
-    // remark 约束
     [self.bottomView mas_remakeConstraints:^(MASConstraintMaker *make) {
         make.height.mas_equalTo(50);
         make.top.mas_equalTo(kScreenWidth-50);
         make.left.equalTo(self.backView).with.offset(0);
         make.width.mas_equalTo(kScreenHeight);
     }];
-    
     
     [self.nowLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(self.slider.mas_left).with.offset(0);
@@ -233,10 +243,8 @@
         weakSelf.backView.transform = CGAffineTransformIdentity;
         weakSelf.backView.frame = CGRectMake(0, 80, kScreenWidth, kScreenHeight / 2.5);
         weakSelf.playerLayer.frame =  weakSelf.backView.bounds;
-        // 再添加到View上
         [weakSelf.view addSubview:weakSelf.backView];
         
-        // remark约束
         [self.bottomView mas_remakeConstraints:^(MASConstraintMaker *make) {
             make.left.equalTo(weakSelf.backView).with.offset(0);
             make.right.equalTo(weakSelf.backView).with.offset(0);
@@ -251,7 +259,7 @@
 
 #pragma mark - 单击手势
 - (void)singleTap:(UITapGestureRecognizer *)tap{
-
+    
     [UIView animateWithDuration:1.0 animations:^{
         if (self.bottomView.alpha == 1)
         {
@@ -262,6 +270,149 @@
             self.bottomView.alpha = 1;
         }
     }];
+}
+
+#pragma mark - slider的更改
+// 不更新视频进度
+- (void)sliderDragValueChange:(UISlider *)slider
+{
+    self.isDragSlider = YES;
+}
+// 点击调用  或者 拖拽完毕的时候调用
+- (void)sliderTapValueChange:(UISlider *)slider
+{
+    self.isDragSlider = NO;
+    // 直接用秒来获取CMTime
+    [self.player seekToTime:CMTimeMakeWithSeconds(slider.value, self.playerItem.currentTime.timescale)];
+}
+
+// 点击Slider
+- (void)touchSlider:(UITapGestureRecognizer *)tap
+{
+    // 根据点击的坐标计算对应的比例
+    CGPoint touch = [tap locationInView:self.slider];
+    CGFloat scale = touch.x / self.slider.bounds.size.width;
+    self.slider.value = CMTimeGetSeconds(self.playerItem.duration) * scale;
+    [self.player seekToTime:CMTimeMakeWithSeconds(self.slider.value, self.playerItem.currentTime.timescale)];
+    if (self.player.rate != 1)
+    {
+        [self.playButton setImage:[UIImage imageNamed:@"pause"] forState:UIControlStateNormal];
+        [self.player play];
+    }
+}
+
+
+// 监听播放器的变化属性
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context
+{
+    if ([keyPath isEqualToString:@"status"])
+    {
+        AVPlayerItemStatus statues = [change[NSKeyValueChangeNewKey] integerValue];
+        switch (statues) {
+            case AVPlayerItemStatusReadyToPlay:
+                self.slider.maximumValue = CMTimeGetSeconds(self.playerItem.duration);
+                [self initTimer];
+                // 自动隐藏底栏
+                if (!self.autoDismissTimer)
+                {
+                    self.autoDismissTimer = [NSTimer timerWithTimeInterval:8.0 target:self selector:@selector(autoDismissView:) userInfo:nil repeats:YES];
+                    [[NSRunLoop currentRunLoop] addTimer:self.autoDismissTimer forMode:NSDefaultRunLoopMode];
+                }
+                break;
+            case AVPlayerItemStatusUnknown:
+                
+                break;
+            case AVPlayerItemStatusFailed:
+                
+                break;
+                
+            default:
+                break;
+        }
+    }
+    // 监听缓存进度的属性
+    else if ([keyPath isEqualToString:@"loadedTimeRanges"])
+    {
+        // 计算缓存进度
+        NSTimeInterval timeInterval = [self availableDuration];
+        // 获取总长度
+        CMTime duration = self.playerItem.duration;
+        
+        CGFloat durationTime = CMTimeGetSeconds(duration);
+        // 监听到了给进度条赋值
+        [self.progressView setProgress:timeInterval / durationTime animated:NO];
+    }
+    
+    
+}
+
+//调用plaer进行UI更新
+- (void)initTimer
+{
+    //player的定时器
+    __weak typeof(self)weakSelf = self;
+    // 每秒更新一次UI Slider
+    [self.player addPeriodicTimeObserverForInterval:CMTimeMake(1, 1) queue:dispatch_get_main_queue() usingBlock:^(CMTime time) {
+        //当前时间
+        CGFloat nowTime = CMTimeGetSeconds(weakSelf.playerItem.currentTime);
+        //总时间
+        CGFloat duration = CMTimeGetSeconds(weakSelf.playerItem.duration);
+        //sec 转换成时间点
+        weakSelf.nowLabel.text = [weakSelf convertToTime:nowTime];
+        weakSelf.remainLabel.text = [weakSelf convertToTime:(duration - nowTime)];
+        //不是拖拽中的话更新UI
+        if (!weakSelf.isDragSlider)
+        {
+            weakSelf.slider.value = CMTimeGetSeconds(weakSelf.playerItem.currentTime);
+        }
+        
+    }];
+}
+//自动隐藏底部功能栏
+- (void)autoDismissView:(NSTimer *)timer{
+    
+    if (self.player.rate == 1)
+    {
+        [UIView animateWithDuration:2.0 animations:^{
+            
+            self.bottomView.alpha = 0;
+            
+        }];
+        
+    }
+}
+
+//计算缓冲进度
+- (NSTimeInterval)availableDuration {
+    NSArray *loadedTimeRanges = [self.playerItem loadedTimeRanges];
+    //获取缓冲区域
+    CMTimeRange timeRange = [loadedTimeRanges.firstObject CMTimeRangeValue];
+    //开始的点
+    float startSeconds = CMTimeGetSeconds(timeRange.start);
+    //已缓存的时间点
+    float durationSeconds = CMTimeGetSeconds(timeRange.duration);
+    //计算缓冲总进度
+    NSTimeInterval result = startSeconds + durationSeconds;
+    return result;
+}
+
+- (NSString *)convertToTime:(CGFloat)time
+{
+    // 初始化格式对象
+    NSDateFormatter *fotmmatter = [[NSDateFormatter alloc] init];
+    // 根据是否大于1H，进行格式赋值
+    if (time >= 3600)
+    {
+        [fotmmatter setDateFormat:@"HH:mm:ss"];
+    }
+    else
+    {
+        [fotmmatter setDateFormat:@"mm:ss"];
+    }
+    // 秒数转换成NSDate类型
+    NSDate *date = [NSDate dateWithTimeIntervalSince1970:time];
+    // date转字符串
+    return [fotmmatter stringFromDate:date];
 }
 
 @end
